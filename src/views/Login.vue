aba3b736a6527b1b4f74ac3650cb88a91b35d540
Md Maruf Ibna Nasim Nishan <nasim15-2328@diu.edu.bd>
2024-03-31 00:42:08 +0600

    login page fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,4 @@
 <template>
-  <div class="container top-0 position-sticky z-index-sticky">
-    <div class="row">
-      <div class="col-12">
-        <navbar
-          is-blur="blur blur-rounded my-3 py-2 start-0 end-0 mx-4 shadow"
-          btn-background="bg-gradient-success"
-          :dark-mode="true"
-        />
-      </div>
-    </div>
-  </div>
   <main class="mt-0 main-content main-content-bg">
     <section>
       <div class="page-header min-vh-75">
@@ -18,92 +7,64 @@
             <div class="mx-auto col-xl-4 col-lg-5 col-md-6 d-flex flex-column">
               <div class="mt-8 card card-plain">
                 <div class="pb-0 card-header text-start">
-                  <h3 class="font-weight-bolder text-success text-gradient text-center">
+                  <h3 class="font-weight-bolder text-success text-gradient">
                     Welcome back
                   </h3>
-                  <br />
-                  <div class="text-center">
-                    <p class="mb-1">You can log in with 1 user types:</p>
-                    <p class="mb-1" style="font-size: 14px">
-                      Username <b>admin@jsonapi.com</b> Password <b>secret</b>
-                    </p>
-                  </div>
+                  <p class="mb-0">Enter your email and password to sign in</p>
                 </div>
-                <div class="card-body pb-3">
-                  <Form
-                    role="form"
-                    class="text-start"
-                    :validation-schema="schema"
-                    @submit="handleLogin"
-                  >
-                    <label for="email">Email</label>
-                    <soft-field
+                <div class="card-body">
+                  <form @submit.prevent="handleLogin" role="form" class="text-start">
+                    <label>Email</label>
+                    <soft-input
                       id="email"
-                      v-model="user.email"
                       type="email"
                       placeholder="Email"
                       name="email"
                     />
-
                     <label>Password</label>
-                    <soft-field
+                    <soft-input
                       id="password"
-                      v-model="user.password"
                       type="password"
                       placeholder="Password"
                       name="password"
                     />
-
                     <soft-switch id="rememberMe" name="rememberMe" checked>
                       Remember me
                     </soft-switch>
-
                     <div class="text-center">
                       <soft-button
                         class="my-4 mb-2"
                         variant="gradient"
                         color="success"
                         full-width
-                        :is-disabled="loading ? true : false"
-                      >
-                        <span
-                          v-if="loading"
-                          class="spinner-border spinner-border-sm"
-                        ></span>
-                        <span v-else>Sign in</span>
+                        >Sign in
                       </soft-button>
                     </div>
-                  </Form>
+                  </form>
                 </div>
-                <div class="px-1 pt-0 pb-3 text-center card-footer px-lg-2">
-                  <p class="mx-auto mb-0 text-sm">
+                <div class="px-1 pt-0 text-center card-footer px-lg-2">
+                  <p class="mx-auto mb-4 text-sm">
                     Don't have an account?
                     <router-link
-                      :to="{ name: 'Register' }"
+                      :to="{ name: 'Sign Up' }"
                       class="text-success text-gradient font-weight-bold"
                       >Sign up</router-link
-                    >
-                  </p>
-                </div>
-                <div class="px-1 pt-0 pb-3 text-center card-footer px-lg-2">
-                  <p class="mx-auto mb-4 text-sm">
-                    Forgot your password?
-                    <router-link
-                      :to="{ name: 'SendEmail' }"
-                      class="text-success text-gradient font-weight-bold"
-                      >Recover</router-link
                     >
                   </p>
                 </div>
               </div>
             </div>
             <div class="col-md-6">
-              <div class="top-0 oblique position-absolute h-100 d-md-block d-none me-n8">
+              <div
+                class="top-0 oblique position-absolute h-100 d-md-block d-none me-n8"
+              >
                 <div
                   class="bg-cover oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"
                   :style="{
                     backgroundImage:
-                      'url(' + require('@/assets/img/curved-images/curved9.jpg') + ')',
+                      'url(' +
+                      require('@/assets/img/curved-images/curved9.jpg') +
+                      ')',
                   }"
                 ></div>
               </div>
@@ -113,30 +74,23 @@
       </div>
     </section>
   </main>
-  <app-footer />
 </template>
 
 <script>
-import Navbar from "@/examples/PageLayout/Navbar.vue";
-import AppFooter from "@/examples/PageLayout/Footer.vue";
-import SoftField from "@/components/SoftField.vue";
+import SoftInput from "@/components/SoftInput.vue";
 import SoftSwitch from "@/components/SoftSwitch.vue";
 import SoftButton from "@/components/SoftButton.vue";
-import showSwal from "/src/mixins/showSwal.js";
 const body = document.getElementsByTagName("body")[0];
 import { mapMutations } from "vuex";
-import { Form } from "vee-validate";
 import * as yup from "yup";
+import showSwal from "/src/mixins/showSwal.js";
 
 export default {
-  name: "Login",
+  name: "SignIn",
   components: {
-    Navbar,
-    AppFooter,
-    SoftField,
+    SoftInput,
     SoftSwitch,
     SoftButton,
-    Form,
   },
 
   data() {
@@ -157,6 +111,7 @@ export default {
       schema,
     };
   },
+
   created() {
     this.toggleEveryDisplay();
     this.toggleHideConfig();
@@ -169,11 +124,12 @@ export default {
   },
   methods: {
     ...mapMutations(["toggleEveryDisplay", "toggleHideConfig"]),
-
     async handleLogin() {
       this.loading = true;
+      console.log(this.user)
       try {
-        await this.$store.dispatch("auth/login", this.user);
+        alert(this.user)
+        //await this.$store.dispatch("auth/login", this.user);
         this.$router.push("/dashboard");
       } catch (error) {
         showSwal.methods.showSwal({
@@ -184,5 +140,6 @@ export default {
       }
     },
   },
+  
 };
 </script>
