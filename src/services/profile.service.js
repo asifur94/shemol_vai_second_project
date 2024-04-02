00586c523cb60c703c388d53db88e30fcf4028ae
Md Maruf Ibna Nasim Nishan <nasim15-2328@diu.edu.bd>
2024-04-03 00:18:37 +0600

    bug fix

--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -8,7 +8,7 @@ import authHeader from "./auth-header";
 const API_URL = 'https://ncare.io';
 export default {
   async getProfile() {
-    const response = await axios.get(API_URL+"api/v1/sg-5/get_auth_user_info/", {
+    const response = await axios.get(API_URL+"/api/v1/sg-5/get_auth_user_info/", {
       headers: authHeader(),
     });
 
