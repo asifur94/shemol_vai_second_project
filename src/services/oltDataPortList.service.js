62f8fff621b27e750ab8173b06ddd179606a8c1a
Md Maruf Ibna Nasim Nishan <nasim15-2328@diu.edu.bd>
2024-04-04 05:05:43 +0600

    bug fix

--- src/services/oltDataPortList.service.js
+++ src/services/oltDataPortList.service.js
@@ -1,18 +1,29 @@
 import axios from "axios";
-let user= JSON.parse(localStorage.getItem("user_data"));
 
 const API_URL = process.env.VUE_APP_API_BASE_URL;
+
 export default {
     async getOltDataPort() {
+      let userPromise = new Promise((resolve, reject) => {
+        let user= JSON.parse(localStorage.getItem("user_data"));
+        if (user) {
+          resolve(user);
+        } else {
+          reject("User not found in localStorage");
+        }
+      });
+  
+      try {
+        const user = await userPromise;
         const response = await axios.get(API_URL+"/api/v1/sg-5/get-Olt_data_port/", {
-            headers: {'Authorization': user.id},
-          });
+          headers: {
+            'Authorization': user.id
+          }
+        });
         return response.data;
+      } catch (error) {
+        console.error("Error retrieving user:", error);
+        throw error; 
+      }
     },
-
-    getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
-    }
-};
+  };
--- src/services/profile.service.js
+++ src/services/profile.service.js
@@ -1,13 +1,28 @@
 import axios from "axios";
 const API_URL = process.env.VUE_APP_API_BASE_URL;
-let token = JSON.parse(localStorage.getItem("userF"));
+
 export default {
   async getProfile() {
-    const response = await axios.get(API_URL+"/api/v1/sg-5/get_auth_user_info/", {
-      headers: {
-        'Authorization': 'Bearer ' + token}
+    let tokenPromise = new Promise((resolve, reject) => {
+      let token = JSON.parse(localStorage.getItem("userF"));
+      if (token) {
+        resolve(token);
+      } else {
+        reject("Token not found in localStorage");
+      }
     });
 
-    return response.data;
+    try {
+      const token = await tokenPromise;
+      const response = await axios.get(API_URL+"/api/v1/sg-5/get_auth_user_info/", {
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error retrieving token:", error);
+      throw error; // Propagate the error to the caller
+    }
   },
 };
